95a3e70781b85d3973d140c9be942fe4bc63fb6c
Aakar Shrestha <amshrestha12@gmail.com>
2021-04-06 15:02:16 -0400

    Using rememberSaveable instead of remember as an internal state to a composable to retained the state during configuration changes or interruptions such as phone calls.

--- swipeToRefresh/src/main/java/com/aakarshrestha/swipetorefresh/SwipeToRefresh.kt
+++ swipeToRefresh/src/main/java/com/aakarshrestha/swipetorefresh/SwipeToRefresh.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -45,10 +46,10 @@ fun SwipeToRefresh(
     content: @Composable () -> Unit
 ) {
 
-    val boxOneOffset = remember { mutableStateOf(0f) }
-    val dragOffset = remember { mutableStateOf(0f) }
-    val refreshTextValue = remember { mutableStateOf("Pull to refresh") }
-    val refreshState = remember { mutableStateOf(isRefreshing) }
+    val boxOneOffset = rememberSaveable { mutableStateOf(0f) }
+    val dragOffset = rememberSaveable { mutableStateOf(0f) }
+    val refreshTextValue = rememberSaveable { mutableStateOf("Pull to refresh") }
+    val refreshState = rememberSaveable { mutableStateOf(isRefreshing) }
 
     val connection = object : NestedScrollConnection {
         override fun onPostScroll(
