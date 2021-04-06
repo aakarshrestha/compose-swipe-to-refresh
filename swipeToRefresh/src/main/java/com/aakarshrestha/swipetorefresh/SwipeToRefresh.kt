6e4327f754499449af2e89114d02d0c3c3bbea39
Aakar Shrestha <amshrestha12@gmail.com>
2021-04-06 15:06:24 -0400

    Removed unwanted import

--- swipeToRefresh/src/main/java/com/aakarshrestha/swipetorefresh/SwipeToRefresh.kt
+++ swipeToRefresh/src/main/java/com/aakarshrestha/swipetorefresh/SwipeToRefresh.kt
@@ -7,7 +7,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
